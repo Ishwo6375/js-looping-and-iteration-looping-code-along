39dcc38fbdedd4a7c8e04a3811c33b144e6e0403
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-04-03 15:24:35 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let newArray = [];
+    let thankYouMessage;
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouMessage = (`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+
+        newArray.push(thankYouMessage);
+    }
+    return newArray;
+}
+
+
+
+function countDown(number) {
+    while (number >=0) {
+        console.log(number);
+        number--;
+    }
+}
